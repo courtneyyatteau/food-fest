c4292ac79239b33975a6085c6eb75d426b2033ad
Courtney Yatteau <courtney.yatteau@gmail.com>
2022-04-18 07:49:14 -0400

    Updates

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,56 +1,76 @@
-import React, { Component, useEffect }  from 'react';
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron, Button, Card, CardBody } from 'reactstrap';
-import { NavLink , Link} from 'react-router-dom';
-import { cleanup, render } from '@testing-library/react';
-import { connect } from 'react-redux';
-import { getNumbers } from './actions/getAction'
-
+import React, { Component, useEffect } from "react";
+import {
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  NavItem,
+  Jumbotron,
+  Button,
+  Card,
+  CardBody,
+} from "reactstrap";
+import { NavLink, Link } from "react-router-dom";
+import { cleanup, render } from "@testing-library/react";
+import { connect } from "react-redux";
+import { getNumbers } from "./actions/getAction";
 
 function Navigation(props) {
-    
-    useEffect(() => {
-        getNumbers();
-    }, [])
+  useEffect(() => {
+    getNumbers();
+  }, []);
 
-    return (
-        <React.Fragment>
-            <Nav navbar className="navItems">
-                <Navbar dark sticky="top" variant="light" >
-                    <div className="container">
-                        <NavbarBrand className="navPic1" >
-                            <Card>
-                                <CardBody className="navPic2"></CardBody>
-                            </Card>
-                        </NavbarBrand>
-                        <NavbarToggler />
-                        <Collapse navbar>
-                                <NavItem>
-                                    <Link className="navLink" to="/">Home </Link>
-                                </NavItem>
-                                <NavItem>
-                                    <Link className="navLink" to="/about">About</Link>
-                                </NavItem>
-                                <NavItem>
-                                    <Link className="navLink" to="/tickets">Tickets</Link>
-                                </NavItem>
-                                <NavItem>
-                                    <Link className="navLink" to="/faq">FAQ</Link>
-                                </NavItem>
-                                <NavItem>
-                                    <Link className="navLink" ><i className="fa fa-shopping-cart"> Cart <span>({props.basketProps.basketNumbers})</span></i></Link>
-                                </NavItem>
-                            
-                        </Collapse>
-                    </div>
-                </Navbar>
-            </Nav>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <Nav navbar className="navItems">
+        <Navbar expand="lg" dark sticky="top" variant="light">
+          <div className="container">
+            <NavbarBrand className="navPic1">
+              <Card>
+                <CardBody className="navPic2"></CardBody>
+              </Card>
+            </NavbarBrand>
+            <NavbarToggler />
+            <Collapse navbar>
+              <NavItem>
+                <Link className="navLink" to="/">
+                  Home{" "}
+                </Link>
+              </NavItem>
+              <NavItem>
+                <Link className="navLink" to="/about">
+                  About
+                </Link>
+              </NavItem>
+              <NavItem>
+                <Link className="navLink" to="/tickets">
+                  Tickets
+                </Link>
+              </NavItem>
+              <NavItem>
+                <Link className="navLink" to="/faq">
+                  FAQ
+                </Link>
+              </NavItem>
+              <NavItem>
+                <Link className="navLink">
+                  <i className="fa fa-shopping-cart">
+                    {" "}
+                    Cart <span>({props.basketProps.basketNumbers})</span>
+                  </i>
+                </Link>
+              </NavItem>
+            </Collapse>
+          </div>
+        </Navbar>
+      </Nav>
+    </React.Fragment>
+  );
 }
 
-
-const mapStateToProps = state => ({
-    basketProps: state.basketState
-})
+const mapStateToProps = (state) => ({
+  basketProps: state.basketState,
+});
 
 export default connect(mapStateToProps, { getNumbers })(Navigation);
--- src/Components/Tickets.js
+++ src/Components/Tickets.js
@@ -1,71 +1,110 @@
-import React, { Component, useState } from 'react';
-import { render } from 'react-dom';
-import { Jumbotron, UncontrolledCollapse, Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
-import Footer from './Footer';
-import { connect } from 'react-redux';
-import { addBasket } from '../Components/actions/addAction';
-import { deleteBasket } from '../Components/actions/deleteAction';
+import React, { Component, useState } from "react";
+import { render } from "react-dom";
+import {
+  Jumbotron,
+  UncontrolledCollapse,
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
+import Footer from "./Footer";
+import { connect } from "react-redux";
+import { addBasket } from "../Components/actions/addAction";
+import { deleteBasket } from "../Components/actions/deleteAction";
 
 const Tickets = (props) => {
-    console.log(props);
-    return (
-      <React.Fragment>
-        <Jumbotron className="TicketJumbo">
-          <div>
-            <Card>
-              <CardTitle className="ticket-head">Ticket Options</CardTitle>
-            </Card>
-          </div>
-          <br />
-          <div className="row">
-            <div className="col-md-6">
+  console.log(props);
+  return (
+    <React.Fragment>
+      <Jumbotron className="TicketJumbo">
+        <div>
+          <Card>
+            <CardTitle className="ticket-head">Ticket Options</CardTitle>
+          </Card>
+        </div>
+        <br />
+        <div className="row">
+          <div className="col-md-6">
             <Card>
               <CardTitle className="TicketsTitle">General Admission</CardTitle>
               <CardBody>
                 <ul>
                   <li>
-                     This ticket gives you access to the Indulgent Fest on either Saturday or Sunday starting at noon.
+                    This ticket gives you access to the Indulgent Fest on either
+                    Saturday or Sunday starting at noon.
                   </li>
                 </ul>
               </CardBody>
             </Card>
-            </div>
-            <div className="col-md-6">
+          </div>
+          <div className="col-md-6">
             <Card>
               <CardTitle className="TicketsTitle">VIP Admission</CardTitle>
               <ul>
                 <li>
-                  This ticket includes first access to the Indulgent Fest starting at 10am on either Saturday or Sunday.
+                  This ticket includes first access to the Indulgent Fest
+                  starting at 10am on either Saturday or Sunday.
                 </li>
                 <li>
-                  The special VIP hour from 10-11AM includes samples, cocktail sips, and a gift bag for shopping that is already filled with a few fun food treats.
+                  The special VIP hour from 10-11AM includes samples, cocktail
+                  sips, and a gift bag for shopping that is already filled with
+                  a few fun food treats.
                 </li>
                 <li>
-                  VIP is limited to 800 people in that exclusive hour, meaning shorter lines, smaller crowds, and more time for you and your friends to INDULGE!
+                  VIP is limited to 800 people in that exclusive hour, meaning
+                  shorter lines, smaller crowds, and more time for you and your
+                  friends to INDULGE!
                 </li>
               </ul>
               <CardBody></CardBody>
             </Card>
-            </div>
           </div>
-          <div className="TicketContainer">
-            <Card className="TicketCard">
-              <h3>General <br />Admission</h3>
-              <h3>$50</h3>
-              <a onClick = {props.addBasket} className="addToCart cart1" href="#">Add to Cart</a>
-              <a onClick = {props.deleteBasket} className="addToCart cart1" href="#">Delete from Cart</a>
-            </Card>
-            <Card className="TicketCard">
-              <h3>VIP<br />Admission</h3>
-              <h3>$80</h3>
-              <a onClick = {props.addBasket} className="addToCart cart2" href="#">Add to Cart</a>
-              <a onClick = {props.deleteBasket} className="addToCart cart1" href="#">Delete from Cart</a>
-            </Card>
-          </div>
-        </Jumbotron>
-        <Footer />
-      </React.Fragment>
-    );
-}
+        </div>
+        <div className="TicketContainer">
+          <Card className="TicketCard">
+            <h3>
+              General <br />
+              Admission
+            </h3>
+            <h3>$50</h3>
+            <a onClick={props.addBasket} className="addToCart cart1" href="#">
+              Add to Cart
+            </a>
+            <br />
+            <a
+              onClick={props.deleteBasket}
+              className="addToCart cart1"
+              href="#"
+            >
+              Delete from Cart
+            </a>
+          </Card>
+          <Card className="TicketCard">
+            <h3>
+              VIP
+              <br />
+              Admission
+            </h3>
+            <h3>$80</h3>
+            <a onClick={props.addBasket} className="addToCart cart2" href="#">
+              Add to Cart
+            </a>
+            <br />
+            <a
+              onClick={props.deleteBasket}
+              className="addToCart cart1"
+              href="#"
+            >
+              Delete from Cart
+            </a>
+          </Card>
+        </div>
+      </Jumbotron>
+      <Footer />
+    </React.Fragment>
+  );
+};
 
-export default connect(null, { addBasket })(Tickets);
+export default connect(null, { addBasket, deleteBasket })(Tickets);
