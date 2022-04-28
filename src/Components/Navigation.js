3c7013057d6908894a61d1b461f79a8c6108a78e
Courtney Yatteau <courtney.yatteau@gmail.com>
2022-04-28 12:16:00 -0400

    Navbar Updates

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Navbar, Container, Nav, Card } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 import { connect } from "react-redux";
@@ -20,44 +20,45 @@ function Navigation(props) {
         collapseOnSelect
         className="navbar-dark"
       >
-        <Container className="container">
-          <Navbar.Brand href="#home" className="navPic1">
-            <Card>
-              <Nav.Link to="/">
-                <div className="navPic2"></div>
-              </Nav.Link>
-            </Card>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link as={Link} className="navLink" to="/" href="/">
-                Home
-              </Nav.Link>
-              <Nav.Link as={Link} className="navLink" to="/about" href="/about">
-                About
-              </Nav.Link>
+        <Navbar.Brand href="#home" className="navPic1">
+          <Nav.Link as={Link} to="/">
+            <div className="navPic2"></div>
+          </Nav.Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link as={Link} className="navLink" to="/" href="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} className="navLink" to="/about" href="/about">
+              About
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              className="navLink"
+              to="/tickets"
+              href="/tickets"
+            >
+              Tickets
+            </Nav.Link>
+            <Nav.Link as={Link} className="navLink" to="/faq" href="/faq">
+              FAQ
+            </Nav.Link>
+            <Link>
               <Nav.Link
                 as={Link}
-                className="navLink"
-                to="/tickets"
-                href="/tickets"
+                className="navLink-cart"
+                to="/cart"
+                href="/cart"
               >
-                Tickets
+                <i className="fa fa-shopping-cart">
+                  Cart <span>({props.basketProps.basketNumbers})</span>
+                </i>
               </Nav.Link>
-              <Nav.Link as={Link} className="navLink" to="/faq" href="/faq">
-                FAQ
-              </Nav.Link>
-              <Link>
-                <Nav.Link as={Link} className="navLink" to="/cart" href="/cart">
-                  <i className="fa fa-shopping-cart">
-                    Cart <span>({props.basketProps.basketNumbers})</span>
-                  </i>
-                </Nav.Link>
-              </Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </React.Fragment>
   );
